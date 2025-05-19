bfd7550f59d8ff532d3615fd1f6deccf63465ede
dcook604 <dcook@spectrum4.ca>
2025-05-19 18:37:11 +0000

    restore

--- shared/schema.ts
+++ shared/schema.ts
@@ -20,6 +20,7 @@ export const users = pgTable("users", {
   passwordResetToken: text("password_reset_token"),
   passwordResetExpires: timestamp("password_reset_expires"),
   forcePasswordChange: boolean("force_password_change").default(false),
+  deletedAt: timestamp("deleted_at"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
